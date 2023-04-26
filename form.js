13a7166c6bccb59b4f129b44d2816383e93fa29c
mia336love <122865270+mia336love@users.noreply.github.com>
2023-04-26 18:40:17 +0300

    form works

--- form.js
+++ form.js
@@ -1,9 +1,35 @@
 // export let students = [];
-let inpName = document.getElementById("#nameId");
-let inpSurame = document.getElementById("#surnameId");
-let inpPatronymic = document.getElementById("#patronymicId");
-let inpBirthday = document.getElementById("#birthdayId");
-let inpYearOfEntry = document.getElementById("#yearOfEntryId");
-let inpFaculty = document.getElementById("#facultyId");
+let inpName = document.getElementById("nameId");
+let inpSurname = document.getElementById("surnameId");
+let inpPatronymic = document.getElementById("patronymicId");
+let inpBirthday = document.getElementById("birthdayId");
+let inpYearOfEntry = document.getElementById("yearOfEntryId");
+let inpFaculty = document.getElementById("facultyId");
 
-let enterBtn = document.getElementById("#enterData");
+let enterBtn = document.getElementById("enterData");
+
+let students = [];
+function addStudent() {
+  let student = {
+    name: inpName.value,
+    surname: inpSurname.value,
+    patronymic: inpPatronymic.value,
+    birthday: inpBirthday.value,
+    yearOfEntry: inpYearOfEntry.value,
+    faculty: inpFaculty.value,
+  };
+
+  //   console.log(student);
+  students.push(student);
+  console.log(students);
+  //   console.log("works");
+
+  inpName.value = "";
+  inpSurname.value = "";
+  inpPatronymic.value = "";
+  inpFaculty.value = "";
+}
+
+if (enterBtn != null) {
+  enterBtn.addEventListener("click", addStudent);
+}
