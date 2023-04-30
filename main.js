0208e92a19ae2ea1ec975b80e0e2d2317af328e7
mia336love <122865270+mia336love@users.noreply.github.com>
2023-04-30 18:16:02 +0300

    form works
    need to redesign

--- main.js
+++ main.js
@@ -14,8 +14,44 @@ const enterBtn = document.getElementById("enterData");
 openForm.addEventListener("click", (e) => {
   e.preventDefault();
   form.classList.add("active");
+  // console.log("active");
 });
 
 closeForm.addEventListener("click", () => {
-  form.classList.add("unactive");
+  form.classList.remove("active");
 });
+
+// if (closeForm != null) {
+//   closeForm.addEventListener("click", () => {
+//     form.classList.remove("active");
+//     console.log("close");
+//   });
+// }
+
+// добавление студентов
+let students = [];
+
+function addStudent() {
+  let student = {
+    name: inpName.value,
+    surname: inpSurname.value,
+    patronymic: inpPatronymic.value,
+    birthday: inpBirthday.value,
+    yearOfEntry: inpYearOfEntry.value,
+    faculty: inpFaculty.value,
+  };
+
+  // console.log(student);
+  students.push(student);
+  console.log(students);
+  // console.log("works");
+
+  inpName.value = "";
+  inpSurname.value = "";
+  inpPatronymic.value = "";
+  inpFaculty.value = "";
+}
+
+if (enterBtn != null) {
+  enterBtn.addEventListener("click", addStudent);
+}
