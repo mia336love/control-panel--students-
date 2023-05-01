7e061ca1adb916f539156812422f3b7bdbc8270b
mia336love <122865270+mia336love@users.noreply.github.com>
2023-05-01 23:49:50 +0300

    update mane

--- main.js
+++ main.js
@@ -22,65 +22,22 @@ const inpFaculty = document.getElementById("facultyId");
 
 const enterBtn = document.getElementById("enterData");
 
-// if (closeForm != null) {
-//   closeForm.addEventListener("click", () => {
-//     form.classList.remove("active");
-//     console.log("close");
-//   });
-// }
-
 // добавление студентов
 let students = [];
 
-// function addStudent() {
-//   let student = {
-//     name: inpName.value,
-//     surname: inpSurname.value,
-//     patronymic: inpPatronymic.value,
-//     birthday: inpBirthday.value,
-//     yearOfEntry: inpYearOfEntry.value,
-//     faculty: inpFaculty.value,
-//   };
-
-//   // console.log(student);
-//   students.push(student);
-//   console.log(students);
-//   // console.log("works");
-
-//   inpName.value = "";
-//   inpSurname.value = "";
-//   inpPatronymic.value = "";
-//   inpFaculty.value = "";
-
-//   let display = `
-//           <tr>
-//             <td>${student.name}</td>
-//             <td>${student.surname}</td>
-//             <td>${student.patronymic}</td>
-//             <td>${student.birthday}</td>
-//             <td>${student.yearOfEntry}</td>
-//             <td>${student.faculty}</td>
-//           </tr>
-//     `;
-//   tbody.insertAdjacentHTML("beforeend", display);
-//   console.log(students);
-// }
-
-// if (enterBtn != null) {
-//   enterBtn.addEventListener("click", addStudent);
-// }
 const tbody = document.querySelector(".tbody");
 
 function addData() {
   let student = {
-    name: inpName.value,
     surname: inpSurname.value,
+    name: inpName.value,
     patronymic: inpPatronymic.value,
     birthday: inpBirthday.value,
     yearOfEntry: inpYearOfEntry.value,
     faculty: inpFaculty.value,
   };
 
+  console.log(student);
   students.push(student);
   let display = `
           
