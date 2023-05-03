b115167eeb23c17688cd06223c58741a2ab20a7c
mia336love <122865270+mia336love@users.noreply.github.com>
2023-05-03 17:24:27 +0300

    aq

--- main.js
+++ main.js
@@ -28,27 +28,84 @@ let students = [];
 const tbody = document.querySelector(".tbody");
 
 function addData() {
+  const birthday = new Date(inpBirthday.value);
+  const birth = birthday.toLocaleDateString();
+
   let student = {
     surname: inpSurname.value,
     name: inpName.value,
     patronymic: inpPatronymic.value,
-    birthday: inpBirthday.value,
+    birthday: birth,
     yearOfEntry: inpYearOfEntry.value,
     faculty: inpFaculty.value,
   };
 
-  console.log(student);
+  // validation();
+  const today = new Date();
+  const now = today.toLocaleDateString();
+
+  // const birthday = new Date(inpBirthday.value);
+  // const birth = birthday.toLocaleDateString();
+
+  console.log(now); //string
+  // console.log(birthday); //string
+  console.log(birth);
+
+  if (birth > now) {
+    alert("false");
+    return;
+  }
+
+  // let timeDiff = Math.floor((now - birth) / (1000 * 3600 * 24));
+  // return timeDiff;
+  // console.log(timeDiff);
+
+  // displayAge(birth);
+
+  let studentAge = Math.floor((now - birth) / (1000 * 60 * 60 * 24 * 30 * 12));
   students.push(student);
   let display = `
           
           <tr>
             <td>${student.name} ${student.surname} ${student.patronymic}</td>
             <td>${student.faculty}</td>
-            <td>${student.birthday}</td>
+            <td>${student.birthday} (${studentAge} лет)</td>
             <td>${student.yearOfEntry}</td>
           </tr>
     `;
   tbody.insertAdjacentHTML("beforeend", display);
+
+  inpSurname.value = "";
+  inpName.value = "";
+  inpPatronymic.value = "";
+  inpBirthday.value = "";
+  inpYearOfEntry.value = "";
+  inpFaculty.value = "";
 }
 enterBtn.addEventListener("click", addData);
 // addData();
+
+function validation() {
+  //проверка на дату рождения
+  // const today = new Date();
+  // const now = today.toLocaleDateString();
+  // const birthday = new Date(inpBirthday.value);
+  // const birth = birthday.toLocaleDateString();
+  // console.log(now); //string
+  // // console.log(birthday); //string
+  // console.log(birth);
+  // if (birth > now) {
+  //   alert("false");
+  //   return;
+  // }
+}
+
+function displayAge(date) {
+  let studentAge = Math.floor(
+    (Date.now() - date) / (1000 * 60 * 60 * 24 * 30 * 12)
+  );
+  return studentAge;
+
+  // let timeDiff = Math.floor((now - birth) / (1000 * 3600 * 24));
+  // return timeDiff;
+}
