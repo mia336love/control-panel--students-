b47948a94c5e7eecb6dc7a7f59f32dae7fef9194
mia336love <122865270+mia336love@users.noreply.github.com>
2023-05-04 15:53:18 +0300

    delete unnecessary stuff

--- main.js
+++ main.js
@@ -32,15 +32,25 @@ function addData() {
   const birthday = formatDate(birth);
   const birthdayX = new Date(inpBirthday.value).toLocaleDateString();
 
+  const year = inpYearOfEntry.value;
+
+  console.log(typeof year);
   let student = {
     surname: inpSurname.value,
     name: inpName.value,
     patronymic: inpPatronymic.value,
     birthday: birthdayX,
-    yearOfEntry: inpYearOfEntry.value,
+    yearOfEntry: year,
     faculty: inpFaculty.value,
   };
 
+  function empty() {
+    if ((inpName = "")) {
+      console.log("empty");
+      return;
+    }
+  }
+  empty();
   // проверка дня рождения + возраст
   const todayDate = new Date();
   const today = formatDate(todayDate);
@@ -53,14 +63,17 @@ function addData() {
   let age = Math.floor((Date.now() - birth) / (1000 * 60 * 60 * 24 * 30 * 12));
 
   // курс
-  let year = students.push(student);
+  let course = Math.trunc(new Date().getFullYear() - inpYearOfEntry.value);
+  course > 4 ? (course = "Завершил") : (course = `${course} курс`);
+
+  students.push(student);
   let display = `
           
           <tr>
             <td>${student.name} ${student.surname} ${student.patronymic}</td>
             <td>${student.faculty}</td>
             <td>${student.birthday} (${age} лет)</td>
-            <td>${student.yearOfEntry}</td>
+            <td>${student.yearOfEntry}-${Number(year) + 4} (${course})</td>
           </tr>
     `;
   tbody.insertAdjacentHTML("beforeend", display);
@@ -75,66 +88,12 @@ function addData() {
 enterBtn.addEventListener("click", addData);
 // addData();
 
-function validation() {}
-
 function displayAge(date) {
   let studentAge = Math.floor(
     (Date.now() - date) / (1000 * 60 * 60 * 24 * 30 * 12)
   );
   return studentAge;
-
-  // let timeDiff = Math.floor((now - birth) / (1000 * 3600 * 24));
-  // return timeDiff;
 }
-
-// function ohLord() {
-//   const today = new Date();
-
-//   const birthday = new Date("2014-12-12");
-
-//   //   function getToday() {
-//   function zeroMonthToday(todayMonth) {
-//     return todayMonth < 10 ? "0" + todayMonth : todayMonth;
-//   }
-
-//   function zeroDayToday(todayDay) {
-//     return todayDay < 10 ? "0" + todayDay : todayDay;
-//   }
-//   const todayYear = today.getFullYear();
-//   const todayMonth = zeroMonthToday(today.getMonth() + 1);
-//   const todayDay = zeroDayToday(today.getDay());
-
-//   const today02 = `${todayYear}.${todayMonth}.${todayDay}`;
-//   console.log(today02);
-//   //   }
-//   //   getToday();
-
-//   //   function getBirthday() {
-//   function zeroMonth(birthMonth) {
-//     return birthMonth < 10 ? "0" + birthMonth : birthMonth;
-//   }
-
-//   function zeroDay(birthDay) {
-//     return birthDay < 10 ? "0" + birthDay : birthDay;
-//   }
-//   const birthYear = birthday.getFullYear();
-//   const birthMonth = zeroMonth(birthday.getMonth() + 1);
-//   const birthDay = zeroDay(birthday.getDay());
-
-//   const birth = `${birthYear}.${birthMonth}.${birthDay}`;
-//   console.log(birth);
-//   //   }
-//   //   getBirthday();
-
-//   if (birth > today02) {
-//     console.log("false");
-//     return;
-//   } else {
-//     console.log("true");
-//   }
-// }
-
-// ohLord();
 
 function formatDate(date) {
   let d = new Date(date),
